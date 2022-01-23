6031e6471015cb116e7c311aff39a90480ef6d5d
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2022-01-24 00:56:15 +0530

    Update alive.py

--- Bawihtei/modules/alive.py
+++ Bawihtei/modules/alive.py
@@ -9,16 +9,16 @@ from Bawihtei.events import register
 from Bawihtei import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/ff2fa22dfa6ae838cc6cd.jpg"
+PHOTO = "https://telegra.ph//file/8272675ce30b994c5ebde.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Bawihtei Robot.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Bawihtei.** \n\n"
   TEXT += "💠 **I'm Working Properly** \n\n"
-  TEXT += f"💠 **My Master : [Vain](https://t.me/saint_foire)** \n\n"
+  TEXT += f"💠 **My Master : [Nicky Lalrochhara](https://t.me/Nickylrca)** \n\n"
   TEXT += f"💠 **Library Version :** `{telever}` \n\n"
   TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Bawihteixbot?start=help"), Button.url("Support", "https://t.me/machinaxsupport")]]
+  BUTTON = [[Button.url("Help", "https://t.me/Bawihteibot?start=help"), Button.url("Support", "https://t.me/BawihteiSupports")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
