aefc7be0cedcf7c25e64cd90fe51ef1ddf0bc1bf
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2022-01-24 21:28:29 +0530

    Update __main__.py

--- Bawihtei/__main__.py
+++ Bawihtei/__main__.py
@@ -170,7 +170,7 @@ def start(update: Update, context: CallbackContext):
                         "pm_help_text"
                     ),
                 )
-            elif args[0].lower().startswith("ghelp_"):
+            def start(update: Update, context: CallbackContext):
                 mod = args[0].lower().split("_", 1)[1]
                 
                 help_list = HELPABLE[mod].helps(update.effective_chat.id)
