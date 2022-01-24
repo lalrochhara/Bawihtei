c646e868d888164ce6fac18e6faeff70f839c7db
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2022-01-24 12:16:43 +0530

    Update config.py

--- Bawihtei/config.py
+++ Bawihtei/config.py
@@ -17,17 +17,17 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 945137470  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "saint_foire"
-    SUPPORT_CHAT = "machinaxsupport"  # Your own group for support, do not add the @
+    OWNER_ID = 1382148141  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Nickylrca"
+    SUPPORT_CHAT = "BawihteiSupports"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001180007354
+        -1001761575985
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001237968047
+        -1001761575985
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     ERROR_LOGS = (
-        -1001732618654
+        -1001761575985
     )  # Prints information Error
 
     # RECOMMENDED
