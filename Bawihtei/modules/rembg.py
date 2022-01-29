5c4a3bc095c9680123e2f4fa037f12efb756b09a
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2022-01-29 12:58:55 +0530

    Update rembg.py

--- Bawihtei/modules/rembg.py
+++ Bawihtei/modules/rembg.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2021 TeamDaisyX
+
+
+# This file is part of Daisy (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import io
 import os
 from datetime import datetime
@@ -6,19 +25,19 @@ import requests
 from telethon import types
 from telethon.tl import functions
 
-from Bawihtei import TEMP_DOWNLOAD_DIRECTORY, telethn
+from Bawihtei import REM_BG_API_KEY
 from Bawihtei.events import register
 from Bawihtei import telethn as tbot
 
-REM_BG_API_KEY = ""
-UNSCREEN_API = ""
+REM_BG_API_KEY = "REM_BG_API_KEY"
+TEMP_DOWNLOAD_DIRECTORY = "./"
 
 
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
         return isinstance(
             (
-                await telethn(functions.channels.GetParticipantRequest(chat, user))
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
             ).participant,
             (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
         )
@@ -26,25 +45,27 @@ async def is_register_admin(chat, user):
         return True
 
 
-@register(pattern="^/rbg")
+@register(pattern="^/rmbg")
 async def _(event):
+    HELP_STR = "use `/rmbg` as reply to a media"
     if event.fwd_from:
         return
-    if event.is_group and not await is_register_admin(
-        event.input_chat, event.message.sender_id
-    ):
-        return
+    if event.is_group:
+        if await is_register_admin(event.input_chat, event.message.sender_id):
+            pass
+        else:
+            return
     if REM_BG_API_KEY is None:
-        await event.reply("You need API token from remove.bg to use this plugin.")
+        await event.reply("`You need API token from remove.bg to use this plugin.`")
         return False
     start = datetime.now()
     message_id = event.message.id
     if event.reply_to_msg_id:
         message_id = event.reply_to_msg_id
         reply_message = await event.get_reply_message()
-        idz = await event.reply("Buaipui mek...")
+        await event.reply("`Processing...`")
         try:
-            downloaded_file_name = await telethn.download_media(
+            downloaded_file_name = await tbot.download_media(
                 reply_message, TEMP_DOWNLOAD_DIRECTORY
             )
         except Exception as e:
@@ -54,14 +75,13 @@ async def _(event):
             output_file_name = ReTrieveFile(downloaded_file_name)
             os.remove(downloaded_file_name)
     else:
-        HELP_STR = "use `/rbg` as reply to a media"
         await event.reply(HELP_STR)
         return
     contentType = output_file_name.headers.get("content-type")
     if "image" in contentType:
         with io.BytesIO(output_file_name.content) as remove_bg_image:
             remove_bg_image.name = "Bawihtei.png"
-            await telethn.send_file(
+            await tbot.send_file(
                 event.chat_id,
                 remove_bg_image,
                 force_document=True,
@@ -74,7 +94,7 @@ async def _(event):
         await event.reply("Background Removed in {} seconds".format(ms))
     else:
         await event.reply(
-            "remove.bg API returned Errors. Please report to @idzeroidsupport\n`{}".format(
+            "remove.bg API returned Errors. Please report to @kenbotsupport\n`{}".format(
                 output_file_name.content.decode("UTF-8")
             )
         )
@@ -87,10 +107,11 @@ def ReTrieveFile(input_file_name):
     files = {
         "image_file": (input_file_name, open(input_file_name, "rb")),
     }
-    return requests.post(
+    r = requests.post(
         "https://api.remove.bg/v1.0/removebg",
         headers=headers,
         files=files,
         allow_redirects=True,
         stream=True,
     )
+    return r
