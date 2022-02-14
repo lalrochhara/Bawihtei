d1eae8768742c6e5aa2c60de5ae5da65ccddad12
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2022-02-14 10:24:42 +0530

    Update feds.py

--- Bawihtei/modules/feds.py
+++ Bawihtei/modules/feds.py
@@ -1417,10 +1417,10 @@ def fed_ban_list(update: Update, context: CallbackContext):
                 backups += json.dumps(json_parser)
                 backups += "\n"
             with BytesIO(str.encode(backups)) as output:
-                output.name = "saitama_fbanned_users.json"
+                output.name = "Bawihtei_fbanned_users.json"
                 update.effective_message.reply_document(
                     document=output,
-                    filename="saitama_fbanned_users.json",
+                    filename="Bawihtei_fbanned_users.json",
                     caption="Total {} User are blocked by the Federation {}.".format(
                         len(getfban),
                         info["fname"],
